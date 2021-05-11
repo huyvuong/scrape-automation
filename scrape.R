6f45f04eaff383e34cd7d639dd8f0da1bd52a731
huyvuong <huy.vuong@gmail.com>
2021-05-10 22:41:38 -0700

    Update scrape.R

--- scrape.R
+++ scrape.R
@@ -3,6 +3,18 @@ library(rvest)
 library(readr)
 
 dat = read_html('https://wol.gg/stats/na/h2cosecretary/')
-scrape = dat %>% html_nodes("p") %>% html_text()
+
+lev = dat %>% html_elements("#level") %>% html_text() %>% parse_number()
+names(lev) = "level"
+
+last_seen = dat %>% html_elements("#last-seen") %>% html_text()
+names(last_seen) = "last_seen"
+
+scrape = dat %>% html_elements("p") %>% html_text()
 vec = parse_number(scrape)
-write_lines(vec, file=paste0("League_", Sys.Date(), ".csv"))
+names(vec) = c("minutes", "hours", "days", "rank_NA", "rank_world", "books", "movies" , "kilometer")
+
+out = c(lev, last_seen, vec)
+
+t(out) %>% as_tibble() %>% write_csv(file=paste0("League_", Sys.Date(), ".csv"))
+
