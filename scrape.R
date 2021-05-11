50cf88f44fc13ec33425ed887c81f073bc1d7bfc
huyvuong <huy.vuong@gmail.com>
2021-05-10 21:37:36 -0700

    Update scrape.R

--- scrape.R
+++ scrape.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(rvest)
 library(readr)
 
-dat = read_html('https://wol.gg/stats/na/h2cosecretary/?fbclid=IwAR2_gj8xyF1lEhZvCRJnp9O6iqGGWq9k_BBdK_O1-vkY83sU7jcWQ3GRmoY')
+dat = read_html('https://wol.gg/stats/na/h2cosecretary/')
 scrape = dat %>% html_nodes("p") %>% html_text()
 vec = parse_number(scrape)
 write_lines(vec, file=paste0("League_", Sys.Date(), ".csv"))
